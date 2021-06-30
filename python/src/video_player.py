ef120f558d976f94c7314ccea15a271f637a3d49
Yash Sharma <yashrsharma44@gmail.com>
2021-07-01 00:43:52 +0530

    Solved the 1st Part - All tests passing
    Signed-off-by: Yash Sharma <yashrsharma44@gmail.com>

--- python/src/video_player.py
+++ python/src/video_player.py
@@ -1,6 +1,10 @@
+import random
+
 """A video player class."""
 
 from .video_library import VideoLibrary
+
+random.seed(23)
 
 
 class VideoPlayer:
@@ -8,6 +12,9 @@ class VideoPlayer:
 
     def __init__(self):
         self._video_library = VideoLibrary()
+        self._map_id_video = dict()
+        self._play_vid_tag = None
+        self._paused_vid_tag = None
 
     def number_of_videos(self):
         num_videos = len(self._video_library.get_all_videos())
@@ -15,8 +22,12 @@ class VideoPlayer:
 
     def show_all_videos(self):
         """Returns all videos."""
-
-        print("show_all_videos needs implementation")
+        print("Here's a list of all available videos:")
+        videos = self._video_library.get_all_videos()
+        videos.sort(key=lambda x: x.title)
+        for video in videos:
+            print("{0} ({1}) [{2}]".format(
+                video.title, video.video_id, ' '.join(video.tags)))
 
     def play_video(self, video_id):
         """Plays the respective video.
@@ -24,32 +35,107 @@ class VideoPlayer:
         Args:
             video_id: The video_id to be played.
         """
-        print("play_video needs implementation")
+        new_video = self._video_library.get_video(video_id)
+        if new_video is None:
+            print("Cannot play video: Video does not exist")
+            return
+
+        if self._play_vid_tag is not None:
+            print("Stopping video: {0}".format(
+                self._video_library.get_video(self._play_vid_tag).title))
+
+        if self._paused_vid_tag is not None:
+            print("Stopping video: {0}".format(
+                self._video_library.get_video(self._paused_vid_tag).title))
+
+        self._play_vid_tag = video_id
+        self._paused_vid_tag = None
+        msg = "Playing video: {0}".format(new_video.title)
+        print(msg)
 
     def stop_video(self):
         """Stops the current video."""
 
-        print("stop_video needs implementation")
+        if self._play_vid_tag is None:
+            print("Cannot stop video: No video is currently playing")
+            return
+
+        self._paused_vid_tag = None
+        print("Stopping video: {0}".format(
+            self._video_library.get_video(self._play_vid_tag).title))
+        self._play_vid_tag = None
 
     def play_random_video(self):
         """Plays a random video from the video library."""
 
-        print("play_random_video needs implementation")
+        if len(self._video_library.get_all_videos()) == 0:
+            print("No videos available")
+            return
+
+        self._paused_vid_tag = None
+        if self._play_vid_tag is not None:
+            print("Stopping video: {0}".format(
+                self._video_library.get_video(self._play_vid_tag).title))
+
+        curr_playing_video = self._video_library.get_all_videos()
+        total_len = len(curr_playing_video)
+        new_video = curr_playing_video[int(random.random()) % total_len]
+
+        self._play_vid_tag = new_video.video_id
+        print("Playing video: {0}".format(new_video.title))
 
     def pause_video(self):
         """Pauses the current video."""
 
-        print("pause_video needs implementation")
+        if self._paused_vid_tag is not None:
+            print("Video already paused: {0}".format(
+                self._video_library.get_video(self._paused_vid_tag).title))
+            return
+
+        if self._play_vid_tag is None:
+            print("Cannot pause video: No video is currently playing")
+            return
+
+        self._paused_vid_tag = self._play_vid_tag
+        self._play_vid_tag = None
+
+        print("Pausing video: {0}".format(
+            self._video_library.get_video(self._paused_vid_tag).title))
 
     def continue_video(self):
         """Resumes playing the current video."""
 
-        print("continue_video needs implementation")
+        if self._play_vid_tag is None and self._paused_vid_tag is None:
+            print("Cannot continue video: No video is currently playing")
+            return
+
+        if self._paused_vid_tag is None:
+            print("Cannot continue video: Video is not paused")
+            return
+
+        # Paused is not None
+        # Play is None
+        self._play_vid_tag = self._paused_vid_tag
+        self._paused_vid_tag = None
+        print("Continuing video: {0}".format(
+            self._video_library.get_video(self._play_vid_tag).title))
 
     def show_playing(self):
         """Displays video currently playing."""
 
-        print("show_playing needs implementation")
+        if self._play_vid_tag is None and self._paused_vid_tag is None:
+            print("No video is currently playing")
+            return
+
+        if self._play_vid_tag is not None:
+            cur_video = self._video_library.get_video(self._play_vid_tag)
+            print("Currently playing: {0} ({1}) [{2}]".format(
+                cur_video.title, cur_video.video_id, ' '.join(cur_video.tags)))
+            return
+
+        paused_video = self._video_library.get_video(self._paused_vid_tag)
+        print("Currently playing: {0} ({1}) [{2}] - PAUSED".format(
+            paused_video.title, paused_video.video_id, ' '.join(paused_video.tags)))
 
     def create_playlist(self, playlist_name):
         """Creates a playlist with a given name.
